c9a3a669009d57929f5fc08798e589454525cea8
Dimitry_Mytnikau <kfifa3@gmail.com>
2024-12-20 22:47:48 +0300

    feat: styles

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { Link } from 'expo-router';
 
 const Index = () => {
   return (
     <View>
+      <Text className="font-bold text-2xl">Welcome to House Rental</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from 'expo-router';
+
+import './global.css';
+
+const RootLayout = () => {
+  return <Stack />;
+};
+
+export default RootLayout;
